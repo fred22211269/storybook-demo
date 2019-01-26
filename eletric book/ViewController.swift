deea0a939412e6f4d80ca9ae2dc538a1bff29b30
fred22211269 <fred22211269@gamil.com>
2019-01-26 16:04:22 +0800

    Initial Commit

---
+++ eletric book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eletric book
+//
+//  Created by 蔡孟甫 on 2019/1/26.
+//  Copyright © 2019 MengFuTsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
